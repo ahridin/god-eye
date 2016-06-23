34ab2e18ff04ac236eae4582d72f8d2980bf8776
TechBK <quangbinh.nguyentrong@gmail.com>
2016-06-23 09:20:11 +0700

    Show exception

--- agent/sendresult.py
+++ agent/sendresult.py
@@ -27,9 +27,9 @@ class SendResult(object):
                 logger.info(
                     "Connected to InfluxDB. Writing data to database...")
                 self.client.write_points(result)
-            except Exception:
+            except Exception as e:
                 logging.exception(
-                    "We get some problems when write points to database")
+                    "We get some problems when write points to database: ", e)
 
 
 class InfluxDBHandler(object):
@@ -47,6 +47,6 @@ class InfluxDBHandler(object):
             logging.info("Connecting to InfluxDB...")
             client = InfluxDBClient(self.host, self.port, self.username,
                                     self.password, self.database)
-        except:
-            logging.exception("Connect to InfuxDB is failed!")
+        except Exception as e:
+            logging.exception("Connect to InfuxDB is failed!: ", e)
         return client
